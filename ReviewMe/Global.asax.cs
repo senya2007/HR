4c6d77d60997480e9281c30c6d0b7dd24b04a17e
senya2007 <penvik19@gmail.com>
2018-05-19 13:05:56 +0400

    Add LightInject DI

--- ReviewMe/Global.asax.cs
+++ ReviewMe/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LightInject;
+using ReviewMe.DAL;
+using ReviewMe.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +14,22 @@ namespace ReviewMe
     {
         protected void Application_Start()
         {
+            InitializeDB.Init();
+
+            var container = new ServiceContainer();
+            container.RegisterApiControllers();
+            container.EnableWebApi(GlobalConfiguration.Configuration);
+
+            RegisterServices(container);
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        private void RegisterServices(ServiceContainer container)
+        {
+            container.Register<IDashboardStatProcessorDAL, DashboardStatProcessorDAL>();
+            container.Register<IDashboardStatProcessorService, DashboardStatProcessorService>();
+            container.Register<ApplicationDbContext>();
         }
     }
 }
