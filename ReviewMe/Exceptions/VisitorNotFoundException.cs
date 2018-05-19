95d7ea3769ee149cc6f1ceb3f7e76e885cab50af
senya2007 <penvik19@gmail.com>
2018-05-19 13:03:01 +0400

    Add VisitorsNotFoundException

---
+++ ReviewMe/Exceptions/VisitorNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReviewMe.Exceptions
+{
+    public class VisitorNotFoundException : Exception
+    {
+        public VisitorNotFoundException(string userName)
+            : base("Visitor not found: " + userName)
+        {
+
+        }
+    }
+}
