e4a82cd2aa819530ecf54edf1dfcfaf3f2a5ee42
senya2007 <penvik19@gmail.com>
2018-05-19 12:57:45 +0400

    Add Services

---
+++ ReviewMe/Services/Dashboardstatprocessor.cs
@@ -0,0 +1,47 @@
+﻿using ReviewMe.Helpers;
+using ReviewMe.Services;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ReviewMe
+{
+    public class DashboardStatProcessorService : IDashboardStatProcessorService
+    {
+        private static IAsyncLock _lock = new AsyncLock();
+
+        IDashboardStatProcessorDAL dashboardDAL;
+
+        public DashboardStatProcessorService(IDashboardStatProcessorDAL dashboard)
+        {
+            dashboardDAL = dashboard;
+        }
+
+        public async Task<int> AddHumanVisitors(string playerName, int value)
+        {
+            using (await _lock.LockAsync())
+            {
+                return await dashboardDAL.AddHumanVisitors(playerName, value);
+            }
+        }
+
+        public async Task<int> GetVisitorsCount(string playerName)
+        {
+            using (await _lock.LockAsync())
+            {
+                return await dashboardDAL.GetVisitorsCount(playerName);
+            }
+        }
+
+        public async Task DeleteVisitorsCount(string playerName)
+        {
+            using (await _lock.LockAsync())
+            {
+                await dashboardDAL.DeleteVisitorsCount(playerName);
+            }
+        }
+    }
+}
---
+++ ReviewMe/Services/IDashboardStatProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ReviewMe.Services
+{
+    public interface IDashboardStatProcessorService
+    {
+        Task<int> AddHumanVisitors(string playerName, int value);
+        Task<int> GetVisitorsCount(string playerName);
+        Task DeleteVisitorsCount(string playerName);
+    }
+}
